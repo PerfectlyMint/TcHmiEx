2defc954a089d0bc1a966f2310d57c412df657f6
Olli Männisto <o.mannisto@beckhoff.fi>
2020-02-25 10:16:08 +0200

    BindSymbolFromArrayIndex.js

---
+++ TcHmiExampleCollection/Scripts/BindSymbolFromArrayIndex.js
@@ -0,0 +1,30 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var BindSymbolFromArrayIndex = function (ctx,ArraySymbol,Index,Control,Property) {
+		// Just editing the symbol expression
+        ArraySymbol.__symbol.__expression.__content = ArraySymbol.__symbol.__expression.__content + "[" + Index + "]";
+        ArraySymbol.__symbol.__expression.__expression = ArraySymbol.__symbol.__expression.__expression.replace("%/s%", "") + "[" + Index + "]%/s%";
+        ArraySymbol.__symbol.__expression.__name = ArraySymbol.__symbol.__expression.__name + "[" + Index + "]";
+
+        // Check if control exists
+        if (Control) {
+            // Bind
+            TcHmi.Binding.createEx2(ArraySymbol.__symbol.__expression.__expression, Property, Control);
+            ctx.success("Result");
+        }
+        else {
+            ctx.error(TcHmi.Errors.ERROR);
+        }
+    };
+
+    TcHmi.Functions.registerFunction('BindSymbolFromArrayIndex', BindSymbolFromArrayIndex);
+})(TcHmi);
